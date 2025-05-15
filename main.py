b36c4f8893fbfd6c283de633ed9e4853cd893d3e
cynorix <65047887+cynorix@users.noreply.github.com>
2025-05-15 18:05:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import os
 from collections import Counter
 from datetime import datetime
+import re
 colorama.init()
 word = requests.get(url=f"https://www.nytimes.com/svc/wordle/v2/{datetime.today().strftime('%Y-%m-%d')}.json").json()['solution']
 guesses = []
@@ -27,9 +28,23 @@ while len(guesses) < 6:
         print("Guess can't contain numbers or symbols!")
     elif len(guess) == 5:
         os.system('cls' if os.name == 'nt' else 'clear')
-        if guess == word:
+        if guess.lower() == word.lower():
             print(f'{Fore.GREEN}{guess}')
-            print('you guessed!')
+            print(f'you guessed!{Fore.RESET}')
+            guesses.append(Fore.GREEN + guess.lower())
+            pattern = re.compile(r'(\x1b\[[0-9;]*m)|([A-Za-z])')
+
+            def replace_letters_keep_colors(text, replacement="██"):
+                def replacer(match):
+                    if match.group(1):
+                        return match.group(1) 
+                    elif match.group(2):
+                        return replacement 
+
+                return pattern.sub(replacer, text)
+
+            for guess in guesses:
+                print(replace_letters_keep_colors(guess))
             input('')
             sys.exit()
         else:
@@ -37,15 +52,15 @@ while len(guesses) < 6:
             word_counter = Counter(word)
             used_letters = Counter()
             for index, char in enumerate(guess):
-                if char in word and char == word[index]:
-                    fguess += Fore.GREEN + char + Fore.RESET
+                if char.lower() in word and char.lower() == word[index]:
+                    fguess += Fore.GREEN + char.lower() + Fore.RESET
                     used_letters[char] += 1
                 elif char in word and used_letters[char] < word_counter[char]:
-                    fguess += Fore.YELLOW + char + Fore.RESET
+                    fguess += Fore.YELLOW + char.lower() + Fore.RESET
                     used_letters[char] += 1
                 else: 
-                    fguess += char
-            guesses.append(fguess)
+                    fguess += char.lower()
+            guesses.append(fguess.lower())
 print(f"""
         {Fore.YELLOW}██████  ██    ██ {Fore.RESET}██████  ██████  ██      ███████ 
         {Fore.YELLOW}██   ██  ██  ██  {Fore.RESET}██   ██ ██   ██ ██      ██      
